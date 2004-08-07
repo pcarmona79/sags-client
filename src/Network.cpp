60b9622ebf08357729cc8484b47cb7cae747a8a9
pcarm <pcarm>
2004-08-07 22:36:33 +0000

    Preparing new version 0.4.0

--- src/Network.cpp
+++ src/Network.cpp
@@ -19,8 +19,8 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //
 // $Source: /home/pablo/Desarrollo/sags-cvs/client/src/Network.cpp,v $
-// $Revision: 1.15 $
-// $Date: 2004/06/30 03:47:07 $
+// $Revision: 1.16 $
+// $Date: 2004/08/07 22:36:34 $
 //
 
 #include <cstdio>
@@ -358,10 +358,10 @@ void *Network::Entry (void)
 				switch (Pkt->GetCommand ())
 				{
 				case Sync::Hello:
-					// ahora manejamos la versión 2
+					// ahora manejamos la versión 3
 					// del protocolo
 					Outgoing << new Packet (Sync::Index, Sync::Version,
-								1, 1, "2");
+								1, 1, "3");
 					send_now = TRUE;
 					break;
 
